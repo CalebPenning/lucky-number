83ec84a44bf3f184f81cc4c414b35d8e6177c405
calebpenning <calebpenning@mail.com>
2021-02-06 13:50:06 -0800

    well i thought it was working

--- star-out-grid.js
+++ star-out-grid.js
@@ -1,19 +1,26 @@
 function starOutGrid(grid) {
-    const starIdx = {}
-	const firstPass = grid.map((row) => {
-    const i = row.findIndex((el) => el === '*')
-    if (i !== -1 && !starIdx[i]) {
-      starIdx[i] = '*';
-      return row.map((el) => (el = '*'));
-    }
-    return row;
-  })
-  return firstPass.map((row) => {
-    return row.map((el, i) => {
-      if (starIdx[i] === '*') {
-        return '*';
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+          if (grid[i + 1] != undefined && grid[i + 1][j] === '*' 
+            || grid[i + 2] != undefined && grid[i + 2][j] === '*') {
+            grid[i][j] = '*'
+          }
+          if (grid[j + 2] != undefined && grid[i][j + 2] === '*') {
+            grid[i][j] = '*'
+          }
+          if (grid[j + 1] != undefined && grid[i][j + 1] === '*') {
+            grid[i][j] = '*'
+          }
+          if (grid[j - 1] != undefined && grid[i][j - 1] === '*' && grid[i][j - 2] == '*') {
+            grid[i][j] = '*'
+          }
+          if (grid[i - 2] != undefined && grid[i - 2][j] === '*') {
+            grid[i][j] = '*'
+          }
+          if (grid[i][j] === '*' && grid[i]) {
+
+          }
       }
-      return el;
-    })
-  })
-}
+    }
+    return grid
+  }
